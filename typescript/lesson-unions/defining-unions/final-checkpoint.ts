35ed53de6556a1cf824f6a709a0f22f085b9ddd1
jonsamp <sampjon@gmail.com>
2020-06-28 10:48:54 -0400

    defining unions exercise

---
+++ typescript/lesson-unions/defining-unions/final-checkpoint.ts
@@ -0,0 +1,6 @@
+function print(statement: string | number) {
+  console.log(`ℹ️ LOG:: ${statement}`);
+}
+
+print("Hiya!");
+print(42);
---
+++ typescript/lesson-unions/defining-unions/initial-checkpoint.ts
@@ -0,0 +1,5 @@
+function print(statement) {
+  console.log(`ℹ️ LOG:: ${statement}`);
+}
+
+print("hello!");
