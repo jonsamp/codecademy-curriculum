346bab3c5e2479c5391869e197214ba1ebd2ad72
jonsamp <sampjon@gmail.com>
2020-07-04 13:02:22 -0400

    common key value pairs

---
+++ typescript/lesson-unions/common-key-value-pairs/final-checkpoint.ts
@@ -0,0 +1,22 @@
+type Like = {
+  username: string;
+  displayName: string;
+};
+
+type Share = {
+  username: string;
+  displayName: string;
+};
+
+function getFriendNameFromEvent(event: Like | Share) {
+  return event.displayName || event.username;
+}
+
+const event = {
+  username: "vkrauss",
+  displayName: "Veronica Krauss",
+};
+
+const name = getFriendNameFromEvent(event);
+
+console.log(`You have an update from ${name}.`);
---
+++ typescript/lesson-unions/common-key-value-pairs/initial-checkpoint.ts
@@ -0,0 +1,21 @@
+type Like = {
+  username: string;
+  displayName: string;
+};
+
+type Share = {
+  username: string;
+};
+
+function getFriendNameFromEvent(event) {
+  return event.displayName || event.username;
+}
+
+const event = {
+  username: "vkrauss",
+  displayName: "Veronica Krauss",
+};
+
+const name = getFriendNameFromEvent(event);
+
+console.log(`You have an update from ${name}.`);
