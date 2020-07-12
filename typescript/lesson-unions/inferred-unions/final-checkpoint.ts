5391f137922a317c4da943d70c52dbf64f61ab42
jonsamp <sampjon@gmail.com>
2020-07-12 16:55:14 -0400

    update to createUser

--- typescript/lesson-unions/inferred-unions/final-checkpoint.ts
+++ typescript/lesson-unions/inferred-unions/final-checkpoint.ts
@@ -3,13 +3,13 @@ type User = {
   username: string;
 };
 
-function getUser() {
+function createUser() {
   const randomChance = Math.random() >= 0.5;
 
   if (randomChance) {
     return { id: 1, username: 'flyingCroissant' };
   } else {
-    return 'Could not retrieve user.';
+    return 'Could not create a user.';
   }
 }
 
