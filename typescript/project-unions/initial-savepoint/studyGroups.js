78743b2007baa71bc3db8511668bd36f93068eb3
jonsamp <sampjon@gmail.com>
2020-07-26 19:22:21 -0400

    commit

---
+++ typescript/project-unions/initial-savepoint/studyGroups.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const studyGroups = [
+    {
+        id: 101,
+        courseId: 1,
+        title: 'Improvisational Arts Lab Study Group',
+        keywords: ['improv', 'art', 'performance', 'lab'],
+        type: 'group',
+    },
+    {
+        id: 102,
+        courseId: 2,
+        title: 'Resarch Methods 1 Study Group',
+        keywords: ['lab', 'research', 'science', 'self-study'],
+        type: 'group',
+    },
+    {
+        id: 103,
+        courseId: 3,
+        title: '19th Century Art Study Group',
+        keywords: ['1800s', 'art', 'history'],
+        type: 'group',
+    },
+];
+exports.default = studyGroups;
